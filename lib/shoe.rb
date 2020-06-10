d6c7c8efca5b366084a4e5059f6dbeef842a90d3
Jack Goode <jack.w.goode@icloud.com>
2020-06-10 23:25:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,14 @@
 class Shoe 
   
   attr_accessor :brand, :color, :size, :material, :condition
-  attr_reader :title
+  attr_reader :brand
   
   def initialize(brand)
      @brand = brand
   end
+ def cobble
+   
+  
+  
+  
+end
